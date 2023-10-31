1efbff115dbeb40d66ff8b9092731101de80c47a
Yohana <yohana@yotec.eti.br>
2023-10-31 20:13:42 -0300

    fix: eureka server configuration

--- order-service/src/main/java/com/yohanaff/github/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/yohanaff/github/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,7 +37,7 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponses = webClient.get()
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
                 .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
