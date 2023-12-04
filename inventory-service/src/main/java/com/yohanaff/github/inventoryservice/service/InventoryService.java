57fa307ce248824d8fc2c2495b183d4bd924ec3d
Yohana <yohana@yotec.eti.br>
2023-12-03 22:13:50 -0300

    feat: implement circuit breaker

--- inventory-service/src/main/java/com/yohanaff/github/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/yohanaff/github/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.yohanaff.github.inventoryservice.service;
 import com.yohanaff.github.inventoryservice.dto.InventoryResponse;
 import com.yohanaff.github.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
--- order-service/src/main/java/com/yohanaff/github/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/yohanaff/github/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -49,6 +49,8 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            return "Order placed successfully";
+
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
